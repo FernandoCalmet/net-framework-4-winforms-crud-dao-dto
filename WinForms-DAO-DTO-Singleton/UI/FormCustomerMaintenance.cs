e8a2d99b3e06ba0c2b5859f5f204db364e6468f1
Fernando Calmet <fercalmet@gmail.com>
2021-11-02 17:23:35 -0500

    more changes

--- WinForms-DAO-DTO-Singleton/UI/FormCustomerMaintenance.cs
+++ WinForms-DAO-DTO-Singleton/UI/FormCustomerMaintenance.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WinForms_DAO_DTO_Singleton.Common;
+using WinForms_DAO_DTO_Singleton.Contracts;
+using WinForms_DAO_DTO_Singleton.DataAccess;
 using WinForms_DAO_DTO_Singleton.Entities;
 
 namespace WinForms_DAO_DTO_Singleton.UI
@@ -15,6 +17,9 @@ namespace WinForms_DAO_DTO_Singleton.UI
     public partial class FormCustomerMaintenance : Form
     {
         #region -> Fields
+        private readonly ICustomerDAO customerData;
+        private Customer customer;
+        private TransactionAction transaction; // Transaction action for persistence.
         private string lastRecord = ""; /*Field to store the last data inserted or edited.
                                           This will allow you to select and view the changes in the datagridview of the Customers form. */
         #endregion
@@ -29,16 +34,67 @@ namespace WinForms_DAO_DTO_Singleton.UI
         #endregion
 
         #region -> Constructor
-        public FormCustomerMaintenance(Customer customer, TransactionAction _transaction)
+        public FormCustomerMaintenance(Customer _customer, TransactionAction _transaction)
         {
             InitializeComponent();
 
+            //Initialize fields
+            customerData = new CustomerDAO();
+            customer = _customer;
+            transaction = _transaction;
 
+            //Initialize control properties
+            InitializeTransactionUI();
         }
         #endregion
 
         #region -> Methods
-
+        private void InitializeTransactionUI()
+        {//This method is responsible for setting the appearance properties based on the action of the transaction.
+            switch (transaction)
+            {
+                case TransactionAction.View:
+                    LastRecord = null;
+                    lblTitle.Text = "Customer details";
+                    lblTitle.ForeColor = Color.MediumSlateBlue;
+                    btnSave.Visible = false;
+                    ReadOnlyFields();
+                    break;
+                case TransactionAction.Add:
+                    lblTitle.Text = "Add new customer";
+                    lblTitle.ForeColor = Color.SeaGreen;
+                    break;
+                case TransactionAction.Edit:
+                    lblTitle.Text = "Edit customer";
+                    lblTitle.ForeColor = Color.RoyalBlue;
+                    break;
+                case TransactionAction.Remove:
+                    lblTitle.Text = "Remove Customer";
+                    lblTitle.ForeColor = Color.IndianRed;
+                    ReadOnlyFields();
+                    break;
+            }
+        }
+        private void ClearFields()
+        {//Clear the form fields.
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtAddress.Clear();
+            txtCity.Clear();
+            txtEmail.Clear();
+            txtPhone.Clear();
+            txtJob.Clear();
+        }
+        private void ReadOnlyFields()
+        {//Make the form fields read-only.
+            txtFirstName.ReadOnly = true;
+            txtLastName.ReadOnly = true;
+            txtAddress.ReadOnly = true;
+            txtCity.ReadOnly = true;
+            txtEmail.ReadOnly = true;
+            txtPhone.ReadOnly = true;
+            txtJob.ReadOnly = true;
+        }
         #endregion
 
         #region -> Event methods
